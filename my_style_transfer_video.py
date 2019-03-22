79b973bbd9ab1bf070257a6cc235538b68c24e4a
Evgeniy <evgen19-97@ya.ru>
2019-03-22 14:45:38 +0300

    change some style

--- my_style_transfer_video.py
+++ my_style_transfer_video.py
@@ -1,19 +1,13 @@
 # import the necessary packages
-from imutils.video import VideoStream
-import glob 
-import itertools
-import time
-import cv2
-from style_transfer import Transfer
-import tensorflow as tf
+import glob
 import os
+import itertools
+import cv2
+from imutils.video import VideoStream
+import tensorflow as tf
 import numpy as np
-"""
-TO DO LIST
-	Rewrite code use pip8
-	Learn which styles we have
-	Take photo
-"""
+from style_transfer import Transfer
+
 
 def load(saver, checkpoint_dir):
 	if os.path.isdir(checkpoint_dir):
@@ -26,20 +20,13 @@ def load(saver, checkpoint_dir):
 	    saver.restore(sess, checkpoint_dir)
 
 
-def saverPicture():
-	pass
-
-
-# initialize the video stream, then allow the camera sensor to warm up
-
 style = glob.glob('checkpoints/*')
-styleIter = itertools.cycle(style) 
+styleIter = itertools.cycle(style)
 checkpoint_dir = next(styleIter)
 
 
 print("[INFO] starting video stream...")
 vs = VideoStream(src=1).start()
-time.sleep(2.0)
 
 frame = vs.read()
 img_shape = frame.shape
@@ -49,11 +36,12 @@ soft_config.gpu_options.allow_growth = True
 
 with g.as_default(), tf.Session(config=soft_config) as sess:
 	print("[INFO] starting session...")
-	img_placeholder = tf.placeholder(tf.float32, shape=[None, *img_shape], name='img_placeholder')
+	img_placeholder = tf.placeholder(
+		tf.float32, shape=[None, *img_shape], name='img_placeholder')
 	model = Transfer()
 	pred = model(img_placeholder)
 	saver = tf.train.Saver()
-	
+
 	load(saver, checkpoint_dir)
 
 	print("[INFO] starting transfer...")
@@ -68,12 +56,12 @@ with g.as_default(), tf.Session(config=soft_config) as sess:
 		cv2.imshow("Output", _pred)
 
 		key = cv2.waitKey(1) & 0xFF
-		
+
 		if key == ord("n"):
 			checkpoint_dir = next(styleIter)
 			load(saver, checkpoint_dir)
 		elif key == ord("s"):
-			cv2.imwrite('photo.jpg',_pred)
+			cv2.imwrite('photo.jpg', _pred)
 		elif key == ord("q"):
 			break
 
